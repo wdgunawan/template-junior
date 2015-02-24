1a33ca0f80aa70f7e6fb1dd9f656cd8e8e839a80
isa9x <iza_helena93@yahoo.co.id>
2015-02-24 16:18:44 +0700

    frenchise
    oi

---
+++ frenchise.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link rel="stylesheet" href="assets/style.css">
+<meta charset="utf-8">
+<title>Dashboard - Bootstrap Admin Template</title>
+<style type="text/css">
+
+</style>
+</head>
+<body>
+	<div class="container">
+		<div class='header'>
+			<div class="logo-container">
+				<div class="logo2"></div>
+				<div class="logo-bg2"></div>
+				
+				<div class="menu-header">
+					<div class="pull-right button-top btn-shadow-red btn-shadow-grey">Contact</div>
+					<div class="pull-right button-top btn-shadow-violet btn-shadow-grey">Member</div>
+					<div class="pull-right button-top btn-shadow-blue">Frenchise</div>
+					<a href="." class="pull-right button-top btn-shadow-green btn-shadow-grey">Home</a>
+				</div>
+			</div>
+		</div>
+		<div class="clear"></div>
+<div class='content_container'>
+		<div class="left" >
+
+		</div>
+		<div class="right">
+			<div class="garis"></div>
+			<div class="content t-justify">
+				<div class="title span6"><font color="00a8eb">Frenchise</font></div>
+				
+				<div class="span4 t-right">
+					<img src="assets/images/foto.jpg" class="pull-right span4 images">
+					</div>
+				
+				</div>
+			<div class="clear"></div>
+			<div class="footer-blue">
+				<div class="content footer-text">
+					<p class="pull-left">Copyright: www.junior.com</p>
+					<div class="pull-right border-left button-bottom btn-red">Contact</div>
+					<div class="pull-right border-left button-bottom btn-violet">Member</div>
+					<div class="pull-right border-left button-bottom btn-blue">Frenchise</div>
+					<a href="." class="pull-right border-left button-bottom btn-green">Home</a>
+				</div>
+				<div class="footer-bg"></div>
+			</div>
+		</div>
+		<div class="clear"></div>
+	</div>
+		<div class="clear"></div>
+	</div>
+
+</body>
+</html>
